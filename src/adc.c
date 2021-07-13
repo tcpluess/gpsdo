534fa748c8366f26820de13a613ddd6e0410f04f
tobias <tpluess@ieee.org>
2021-07-13 11:43:30 +0200

    specify maximum adc wait time

--- src/adc.c
+++ src/adc.c
@@ -48,6 +48,9 @@
 /* gain of the INA193 */
 #define GAIN 20.0f
 
+/* maximum time to wait for the ADC */
+#define ADC_MAX_WAITDELAY 10u /* ms */
+
 /*******************************************************************************
  * PRIVATE MACRO DEFINITIONS
  ******************************************************************************/
@@ -112,7 +115,7 @@ float get_iocxo(void)
 {
   /* wait maximum 10ms for the ADC if, for some reason, the adc has not been
      properly triggered */
-  if(xSemaphoreTake(adc_ready, portMAX_DELAY))
+  if(xSemaphoreTake(adc_ready, pdMS_TO_TICKS(ADC_MAX_WAITDELAY)))
   {
     /* these are the internal reference voltage and the ocxo current */
     float ichan = ADC1_JDR1;
