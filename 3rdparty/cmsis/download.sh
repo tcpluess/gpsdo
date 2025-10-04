6e2534d6508007ca0a67a52dc64fe915e8a5eee1
tobias <tpluess@ieee.org>
2025-10-04 16:17:25 +0200

    updatre cmsis and stm32 headers

--- 3rdparty/cmsis/download.sh
+++ 3rdparty/cmsis/download.sh
@@ -1,8 +1,8 @@
 #/bin/bash
-VERSION=5.9.0
+VERSION=6.2.0
 
 get () {
-  curl --create-dirs https://raw.githubusercontent.com/ARM-software/CMSIS_5/$VERSION/$1 -o $1
+  curl --create-dirs https://raw.githubusercontent.com/ARM-software/CMSIS_6/refs/tags/v$VERSION/$1 -o $1
 }
 
 get "LICENSE.txt"
@@ -10,4 +10,7 @@ get "CMSIS/Core/Include/cmsis_compiler.h"
 get "CMSIS/Core/Include/cmsis_gcc.h"
 get "CMSIS/Core/Include/cmsis_version.h"
 get "CMSIS/Core/Include/core_cm4.h"
-get "CMSIS/Core/Include/mpu_armv7.h"
+get "CMSIS/Core/Include/m-profile/cmsis_gcc_m.h"
+get "CMSIS/Core/Include/m-profile/armv7m_mpu.h"
+get "CMSIS/Core/Include/m-profile/armv7m_cachel1.h"
+
